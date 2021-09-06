0f424c4eb8b0643d6777da7fce31341fc5c10624
Andre Viallon <viallon.andre@gmail.com>
2021-09-06 21:18:31 +0200

    chore: get all tasks service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Task } from 'src/task.interface';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getTasks(): Task[] {
+    return this.tasksService.getTasks();
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from 'src/task.interface';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [
+    { title: 'Drink Coffee' },
+    { title: 'Learn Nest.js' },
+  ];
+
+  public getTasks(): Task[] {
+    return this.tasks;
+  }
+}
