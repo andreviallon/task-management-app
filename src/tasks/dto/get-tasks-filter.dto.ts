65f8adcfa613e139671a94d641948a0966906c05
Andre Viallon <viallon.andre@gmail.com>
2021-10-29 11:51:01 +0200

    chore: get tasks filter validations

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TaskStatusEnum } from '../../task.interface';
 
 export class GetTasksFilterDto {
-  status?: TaskStatusEnum;
-  search?: string;
+  @IsOptional()
+  @IsEnum(TaskStatusEnum)
+  public status?: TaskStatusEnum;
+
+  @IsOptional()
+  @IsString()
+  public search?: string;
 }
