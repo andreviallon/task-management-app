0074f549b77a137d1596a6490f7bb65aa16a68b3
Andre Viallon <viallon.andre@gmail.com>
2021-10-30 13:38:58 +0200

    chore: create task entity

---
+++ src/tasks/task.entity.ts
@@ -0,0 +1,17 @@
+import { TaskStatusEnum } from 'src/task.interface';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: TaskStatusEnum;
+}
