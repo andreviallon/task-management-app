e066e24f02b05ee261ddbc8f03820d9afb213d8e
Andre Viallon <viallon.andre@gmail.com>
2021-11-03 18:16:56 +0100

    chore: create task logic

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,5 +1,17 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { Task } from './task.entity';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskStatusEnum } from '../task-status.interface';
 
 @EntityRepository(Task)
-export class TasksRepository extends Repository<Task> {}
+export class TasksRepository extends Repository<Task> {
+  public createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+
+    return this.create({
+      title,
+      description,
+      status: TaskStatusEnum.OPEN,
+    });
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './task.entity';
 import { TasksRepository } from './task.repository';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -15,6 +16,17 @@ export class TasksService {
       return await this.tasksRepository.findOne(id);
     } catch (error) {
       throw new NotFoundException(`Task with id '${id}' not found`);
+    }
+  }
+
+  public async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    const task = this.tasksRepository.createTask(createTaskDto);
+
+    try {
+      await this.tasksRepository.save(task);
+      return task;
+    } catch (error) {
+      throw new NotFoundException('Task could not be saved... :(');
     }
   }
 }
