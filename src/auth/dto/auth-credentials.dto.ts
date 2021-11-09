b63d813ae5e063b0f8f3e11d79019a0c2f00a83c
Andre Viallon <viallon.andre@gmail.com>
2021-11-09 17:44:50 +0100

    chore: username and password requirements

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,22 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  /*
+  Ensure that password contains at least:
+    - 1 upper case letter
+    - 1 lower case letter
+    - 1 number or special character
+  */
+  @Matches(/((?=.*d)|(?=.*W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Password must have at least 1 upper case letter, 1 lower case letter and 1 number or special character',
+  })
   password: string;
 }
