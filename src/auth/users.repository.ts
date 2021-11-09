f82bf0bdf4c99114d8d4dca3f2f0fab95d1c662d
Andre Viallon <viallon.andre@gmail.com>
2021-11-09 17:58:29 +0100

    chore: make user unique

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,12 +1,18 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import { UserError } from './userError.util';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
     const user = this.create({ username, password });
-    await this.save(user);
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      UserError.handleUserError(error);
+    }
   }
 }
