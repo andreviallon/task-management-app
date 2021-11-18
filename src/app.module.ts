742fabc63b0788c276ac114705afa03ac23e9ae8
Andre Viallon <viallon.andre@gmail.com>
2021-11-18 19:53:13 +0100

    chore: move jwt_secret to env

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,18 +17,16 @@ import { configValidationSchema } from './config.schema';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => {
-        return {
-          autoLoadEntities: true,
-          synchronize: true,
-          type: 'postgres',
-          host: configService.get('DB_HOST'),
-          port: configService.get('DB_PORT'),
-          username: configService.get('DB_USERNAME'),
-          password: configService.get('DB_PASSWORD'),
-          database: configService.get('DB_NAME'),
-        };
-      },
+      useFactory: async (configService: ConfigService) => ({
+        autoLoadEntities: true,
+        synchronize: true,
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+      }),
     }),
     AuthModule,
   ],
