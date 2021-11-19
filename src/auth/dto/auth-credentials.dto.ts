c3ab80f2ff5b861f84bfe927d1701489a0bba1a1
Andre Viallon <viallon.andre@gmail.com>
2021-11-19 17:27:50 +0100

    chore: remove comments

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -8,11 +8,5 @@ export class AuthCredentialsDto {
 
   @IsString()
   @MinLength(8, { message: 'Password must be at least 8 characters long' })
-  /*
-  Ensure that password contains at least:
-    - 1 upper case letter
-    - 1 lower case letter
-    - 1 number or special character
-  */
   password: string;
 }
